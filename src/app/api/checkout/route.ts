c23c80e8187beaab84d9a4ba28531eebfb9cb2ae
Alfrantor <alfredotoralla@gmail.com>
2026-03-11 22:08:46 -0600

    Fix: Versión de API de Stripe

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 // Inicializamos Stripe con la clave secreta
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2025-02-24.acacia" as any,
 });
 
 export async function POST(request: Request) {
