aafd1b574be2283b244677e1332e63f6e3f1ce5b
Alfrantor <alfredotoralla@gmail.com>
2026-03-31 15:06:14 -0600

    pos4

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -212,7 +212,7 @@ export default async function AdminPage({ searchParams }: AdminPageProps) {
     }
 
     // ==========================================
-    // 4. HISTORIAL DE PRECIOS    detalles en los precios
+    // 4. HISTORIAL DE PRECIOS
     // ==========================================
     let priceHistory = [];
 
